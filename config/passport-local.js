9827488fd4998ef7a736457f3e6d1a471059c806
Tewodros Abebe <teddyab729@gmail.com>
2023-11-24 10:11:36 +0300

    Google-OAuth-Strategy

---
+++ config/passport-local.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("../models/User");
+const { compare } = require("bcrypt");
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    async (req, email, password, done) => {
+      try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user || !(await compare(password, user.password))) {
+          return done(null, false, { message: "Incorrect Email or Password" });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+module.exports = LocalStrategy;
